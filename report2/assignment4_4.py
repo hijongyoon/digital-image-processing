95d764e17eea5b9d3d846feeb01c97b9d9094d52
hijongyoon <darkknight35@nate.com>
2020-10-31 12:08:21 +0900

    image-sharpening

---
+++ report2/assignment4_4.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def check(num):
+    if num > 255:
+        num = 255
+    elif num < 0:
+        num = 0
+    return num
+
+
+def make_sharpening(img, n):
+    mask = [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]
+    img = np.pad(img, pad_width=1, mode='constant', constant_values=0)  # 0 패딩
+    new_img = np.zeros((column, row), dtype=np.uint8)
+    s = 0.0
+    for i in range(1, column + 1):
+        for j in range(1, row + 1):
+            for x in range(n):
+                for z in range(n):
+                    s += mask[x][z] * img[i + x - 1][j + z - 1]
+            new_img[i - 1][j - 1] = check(s)
+            s = 0.0
+    return new_img
+
+
+img = cv2.imread("../image/test_img23.png", cv2.IMREAD_GRAYSCALE)
+column = len(img)
+row = len(img[0])
+cv2.imshow("Before", img)
+cv2.imshow("After", make_sharpening(img, 3))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
